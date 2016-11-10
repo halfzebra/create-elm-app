2aae06fead82ffb05920c14dbaa11b1f7adcc95e
Alexander Biryukov <alex.biryukoff@gmail.com>
2016-11-10 10:11:47 +0700

    Add test case with returnin non zero exit codes

---
+++ tests/data/Main.elm
@@ -0,0 +1,9 @@
+module Main exposing (..)
+
+import App exposing (..)
+import Html.App exposing (program)
+
+
+main : Program Never
+main 
+    program { view = view, init = init, update = update, subscriptions = subscriptions }
