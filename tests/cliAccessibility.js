5ac410b794efaae8d47c06ce8b3208ea42947fdd
Alexander Biryukov <alex.biryukoff@gmail.com>
2016-11-14 03:03:33 +0700

    Refactor tests. Fixes #45 and fixes #47 (#53)

---
+++ tests/cliAccessibility.js
@@ -0,0 +1,36 @@
+const expect = require('chai').expect;
+const spawn = require('cross-spawn');
+const rimraf = require('rimraf');
+
+const testAppName = 'test-app';
+
+describe('Test command line interface functionality', function () {
+  after(function () {
+    rimraf.sync(testAppName);
+  });
+
+  describe('CLI accessibility', function () {
+    it('`create-elm-app` command should be available', function () {
+      var output = spawn.sync('create-elm-app').output;
+      expect(output.toString()).to.have.string('Usage: create-elm-app <project-directory>');
+    });
+
+    it('`elm-app` command should be available', function () {
+      var result = spawn.sync('elm-app');
+      var output = result.output;
+      expect(output.toString()).to.have.string('Usage: elm-app <command>');
+    });
+
+    it('`elm-app package` command should be available', function () {
+      var result = spawn.sync('elm-app', [ 'package' ]);
+      var output = concatsStringBuffers(result.output);
+      expect(output).to.have.string('install and publish elm packages');
+    });
+  });
+});
+
+function concatsStringBuffers (buffers) {
+  return buffers.map(function (out) {
+    return (out !== null) ? out.toString() : '';
+  }).join('');
+}
