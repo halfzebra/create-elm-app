3329bb66e80c1596d28472ef20722fd78cfddaba
Lucas Rosa <lucas.rosa@kipusystems.com>
2017-05-05 14:31:57 -0400

    attempt to keep src folder pure elm

--- config/paths.js
+++ config/paths.js
@@ -4,10 +4,10 @@ const appRoot = process.cwd()
 
 let paths = {
   appRoot,
-  entry: path.resolve('./src/index.js'),
+  entry: path.resolve('./public/index.js'),
   dist: path.resolve('./dist'),
-  template: path.resolve('./src/index.html'),
-  favicon: path.resolve('./src/favicon.ico'),
+  template: path.resolve('./public/index.html'),
+  favicon: path.resolve('./public/favicon.ico'),
   elmPkg: path.resolve('elm-package.json'),
   elmMake: require('elm/platform').executablePaths['elm-make'],
   servedPath: './' || process.env.SERVED_PATH
