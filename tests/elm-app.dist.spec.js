bec2247e3bd725c29b60db9388857493f2c722b9
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-05-27 20:46:14 +0200

    test(build): Added a test for ensuring that application compiles correctly
    Added Browser test to check if application is compiled and produces the expected output

---
+++ tests/elm-app.dist.spec.js
@@ -0,0 +1,52 @@
+/* eslint-disable */
+/* eslint-env mocha */
+const fs = require('fs')
+const path = require('path')
+const spawn = require('cross-spawn')
+const rimraf = require('rimraf')
+const expect = require('chai').expect
+const Nightmare = require('nightmare')
+
+const testAppName = 'test-app'
+const rootDir = path.resolve(__dirname, '..')
+const testAppDir = path.join(rootDir, testAppName)
+const createElmAppCmd = path.join(rootDir, 'bin/create-elm-app-cli.js')
+const elmAppCmd = path.join(rootDir, 'bin/elm-app-cli.js')
+
+describe('Creating and making a dist build of Elm application', function () {
+  this.timeout(10000)
+
+  before(done => {
+    spawn('node', [createElmAppCmd, testAppName]).on('close', status => {
+      if (status === 0) {
+        spawn('node', [elmAppCmd, 'build'], {cwd: testAppDir}).on('close', status => {
+          if (status === 0) {
+            done()
+          }
+        })
+      } else {
+        done(false)
+      }
+    })
+  })
+
+  after(() => {
+    rimraf.sync(testAppDir)
+  })
+
+  it('compiled correctly and renders "Your Elm App is working!" text', done => {
+    Nightmare()
+      .goto('file://' + path.resolve(testAppDir, 'dist/index.html'))
+      .evaluate(() => {
+        return document.body.innerText
+      })
+      .end()
+      .then((result) => {
+        expect(result.trim()).to.be.equal('Your Elm App is working!')
+        done()
+      })
+      .catch(err => {
+        done(err)
+      })
+  })
+})
