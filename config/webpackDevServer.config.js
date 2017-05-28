2d2bd85ce9f69896ee3f1fefece6b14d7b58ddfd
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-05-28 04:28:04 +0200

    style: Fixed code style in the config for the dev server

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -63,7 +63,7 @@ module.exports = function (proxy, allowedHost) {
     // Reportedly, this avoids CPU overload on some systems.
     // https://github.com/facebookincubator/create-react-app/issues/293
     watchOptions: {
-      ignored: /node_modules/,
+      ignored: /node_modules/
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === 'https',
@@ -72,11 +72,11 @@ module.exports = function (proxy, allowedHost) {
     historyApiFallback: {
       // Paths with dots should still use the history fallback.
       // See https://github.com/facebookincubator/create-react-app/issues/387.
-      disableDotRule: true,
+      disableDotRule: true
     },
     public: allowedHost,
     proxy,
-    setup(app) {
+    setup (app) {
       // This lets us open files from the runtime error overlay.
       app.use(errorOverlayMiddleware())
       // This service worker file is effectively a 'no-op' that will reset any
@@ -85,6 +85,6 @@ module.exports = function (proxy, allowedHost) {
       // it used the same host and port.
       // https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
       app.use(noopServiceWorkerMiddleware())
-    },
+    }
   }
 }
