ce7917749a64f6d211a7d3bdb2ab814ea882a5a7
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-05-28 02:30:01 +0200

    feat(scripts): Improved the start script
    The start script now supports the option to use a different port and has better messages
    
    Closes #36

--- scripts/start.js
+++ scripts/start.js
@@ -7,164 +7,168 @@ const chalk = require('chalk')
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const config = require('../config/webpack.config.dev')
+const {choosePort, prepareUrls, prepareProxy} = require('react-dev-utils/WebpackDevServerUtils')
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages')
 const clearConsole = require('react-dev-utils/clearConsole')
 const openBrowser = require('react-dev-utils/openBrowser')
-const historyApiFallback = require('connect-history-api-fallback')
-const httpProxyMiddleware = require('http-proxy-middleware')
+const createDevServerConfig = require('../config/webpackDevServer.config')
+const paths = require('../config/paths')
 
 if (fs.existsSync('elm-package.json') === false) {
   console.log('Please, run the build script from project root directory')
   process.exit(0)
 }
 
-// http://webpack.github.io/docs/node.js-api.html#the-long-way
-const compiler = webpack(config)
-const port = 3000
+const isInteractive = process.stdout.isTTY
 
-compiler.plugin('invalid', function () {
-  clearConsole()
-  console.log('Compiling...')
-})
+function printInstructions (appName, urls) {
+  console.log()
+  console.log(`You can now view ${chalk.bold(appName)} in the browser.`)
+  console.log()
 
-compiler.plugin('done', function (stats) {
-  clearConsole()
-
-  const hasErrors = stats.hasErrors()
-  const hasWarnings = stats.hasWarnings()
-
-  if (!hasErrors && !hasWarnings) {
-    console.log(chalk.green('Compiled successfully!'))
-    console.log('\nThe app is running at:')
-    console.log('\n    ' + chalk.cyan('http://localhost:' + port + '/'))
-    console.log('\nTo create production build, run:')
-    console.log('\n    elm-app build')
-    return
-  }
-
-  if (hasErrors) {
-    console.log(chalk.red('Compilation failed.\n'))
-
-    const json = formatWebpackMessages(stats.toJson({}, true))
-
-    json.errors.forEach(function (message) {
-      console.log(message)
-      console.log()
-    })
-  }
-})
-
-// We need to provide a custom onError function for httpProxyMiddleware.
-// It allows us to log custom error messages on the console.
-function onProxyError (proxy) {
-  return function (err, req, res) {
-    const host = req.headers && req.headers.host
+  if (urls.lanUrlForTerminal) {
     console.log(
-      chalk.red('Proxy error:') + ' Could not proxy request ' + chalk.cyan(req.url) +
-      ' from ' + chalk.cyan(host) + ' to ' + chalk.cyan(proxy) + '.'
+      `  ${chalk.bold('Local:')}            ${urls.localUrlForTerminal}`
     )
     console.log(
-      'See https://nodejs.org/api/errors.html#errors_common_system_errors for more information (' +
-      chalk.cyan(err.code) + ').'
+      `  ${chalk.bold('On Your Network:')}  ${urls.lanUrlForTerminal}`
     )
+  } else {
+    console.log(`  ${urls.localUrlForTerminal}`)
+  }
+
+  console.log()
+  console.log('Note that the development build is not optimized.')
+  console.log(
+    `To create a production build, use ` +
+    `${chalk.cyan('elm-app build')}.`
+  )
+  console.log()
+}
+
+function createCompiler (webpack, config, appName, urls) {
+  // "Compiler" is a low-level interface to Webpack.
+  // It lets us listen to some events and provide our own custom messages.
+  let compiler
+  try {
+    compiler = webpack(config)
+  } catch (err) {
+    console.log(chalk.red('Failed to compile.'))
     console.log()
+    console.log(err.message || err)
+    console.log()
+    process.exit(1)
+  }
 
-    // And immediately send the proper error response to the client.
-    // Otherwise, the request will eventually timeout with ERR_EMPTY_RESPONSE on the client side.
-    if (res.writeHead && !res.headersSent) {
-      res.writeHead(500)
+  // "invalid" event fires when you have changed a file, and Webpack is
+  // recompiling a bundle. WebpackDevServer takes care to pause serving the
+  // bundle, so if you refresh, it'll wait instead of serving the old one.
+  // "invalid" is short for "bundle invalidated", it doesn't imply any errors.
+  compiler.plugin('invalid', () => {
+    if (isInteractive) {
+      clearConsole()
     }
-    res.end('Proxy error: Could not proxy request ' + req.url + ' from ' +
-      host + ' to ' + proxy + ' (' + err.code + ').'
+    console.log('Compiling...')
+  })
+
+  let isFirstCompile = true
+
+  // "done" event fires when Webpack has finished recompiling the bundle.
+  // Whether or not you have warnings or errors, you will get this event.
+  compiler.plugin('done', stats => {
+    if (isInteractive) {
+      clearConsole()
+    }
+
+    // We have switched off the default Webpack output in WebpackDevServer
+    // options so we are going to "massage" the warnings and errors and present
+    // them in a readable focused way.
+    const messages = formatWebpackMessages(stats.toJson({}, true))
+    const isSuccessful = !messages.errors.length && !messages.warnings.length
+    if (isSuccessful) {
+      console.log(chalk.green('Compiled successfully!'))
+    }
+    if (isSuccessful && (isInteractive || isFirstCompile)) {
+      printInstructions(appName, urls)
+    }
+    isFirstCompile = false
+
+    // If errors exist, only show errors.
+    if (messages.errors.length) {
+      console.log(chalk.red('Failed to compile.\n'))
+      console.log(messages.errors.join('\n\n'))
+      return
+    }
+
+    // Show warnings if no errors were found.
+    if (messages.warnings.length) {
+      console.log(chalk.yellow('Compiled with warnings.\n'))
+      console.log(messages.warnings.join('\n\n'))
+
+      // Teach some ESLint tricks.
+      console.log(
+        '\nSearch for the ' +
+        chalk.underline(chalk.yellow('keywords')) +
+        ' to learn more about each warning.'
+      )
+      console.log(
+        'To ignore, add ' +
+        chalk.cyan('// eslint-disable-next-line') +
+        ' to the line before.\n'
+      )
+    }
+  })
+  return compiler
+}
+
+// Tools like Cloud9 rely on this.
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000
+const HOST = process.env.HOST || '0.0.0.0'
+
+// We attempt to use the default port but if it is busy, we offer the user to
+// run on a different port. `detect()` Promise resolves to the next free port.
+choosePort(HOST, DEFAULT_PORT)
+  .then(port => {
+    if (port == null) {
+      // We have not found a port.
+      return
+    }
+    const protocol = process.env.HTTPS === 'true' ? 'https' : 'http'
+    const appName = require(paths.elmPkg).name
+    const urls = prepareUrls(protocol, HOST, port)
+    // Create a webpack compiler that is configured with custom messages.
+    const compiler = createCompiler(webpack, config, appName, urls)
+    // Load proxy config
+    const proxySetting = require(paths.elmPkg).proxy
+    const proxyConfig = prepareProxy(proxySetting, '/')
+    // Serve webpack assets generated by the compiler over a web sever.
+    const serverConfig = createDevServerConfig(
+      proxyConfig,
+      urls.lanUrlForConfig
     )
-  }
-}
+    const devServer = new WebpackDevServer(compiler, serverConfig)
+    // Launch WebpackDevServer.
+    devServer.listen(port, HOST, err => {
+      if (err) {
+        return console.log(err)
+      }
+      if (isInteractive) {
+        clearConsole()
+      }
+      console.log(chalk.cyan('Starting the development server...\n'))
+      openBrowser(urls.localUrlForBrowser)
+    });
 
-function addMiddleware (devServer) {
-  // `proxy` lets you to specify a fallback server during development.
-  // Every unrecognized request will be forwarded to it.
-  const proxy = JSON.parse(fs.readFileSync('elm-package.json', 'utf-8')).proxy
-  devServer.use(historyApiFallback({
-    // Paths with dots should still use the history fallback.
-    // See https://github.com/facebookincubator/create-react-app/issues/387.
-    disableDotRule: true,
-    // For single page apps, we generally want to fallback to /index.html.
-    // However we also want to respect `proxy` for API calls.
-    // So if `proxy` is specified, we need to decide which fallback to use.
-    // We use a heuristic: if request `accept`s text/html, we pick /index.html.
-    // Modern browsers include text/html into `accept` header when navigating.
-    // However API calls like `fetch()` won’t generally accept text/html.
-    // If this heuristic doesn’t work well for you, don’t use `proxy`.
-    htmlAcceptHeaders: proxy
-      ? [ 'text/html' ]
-      : [ 'text/html', '*/*' ]
-  }))
-  if (proxy) {
-    if (typeof proxy !== 'string') {
-      console.log(chalk.red('When specified, "proxy" in package.json must be a string.'))
-      console.log(chalk.red('Instead, the type of "proxy" was "' + typeof proxy + '".'))
-      console.log(chalk.red('Either remove "proxy" from package.json, or make it a string.'))
-      process.exit(1)
-    }
-
-    // Otherwise, if proxy is specified, we will let it handle any request.
-    // There are a few exceptions which we won't send to the proxy:
-    // - /index.html (served as HTML5 history API fallback)
-    // - /*.hot-update.json (WebpackDevServer uses this too for hot reloading)
-    // - /sockjs-node/* (WebpackDevServer uses this for hot reloading)
-    // Tip: use https://jex.im/regulex/ to visualize the regex
-    const mayProxy = /^(?!\/(index\.html$|.*\.hot-update\.json$|sockjs-node\/)).*$/
-
-    // Pass the scope regex both to Express and to the middleware for proxying
-    // of both HTTP and WebSockets to work without false positives.
-    const hpm = httpProxyMiddleware(
-      function (pathname) {
-        return mayProxy.test(pathname)
-      },
-      {
-        target: proxy,
-        logLevel: 'silent',
-        onProxyReq: function (proxyReq) {
-          // Browers may send Origin headers even with same-origin
-          // requests. To prevent CORS issues, we have to change
-          // the Origin to match the target URL.
-          if (proxyReq.getHeader('origin')) {
-            proxyReq.setHeader('origin', proxy)
-          }
-        },
-        onError: onProxyError(proxy),
-        secure: false,
-        changeOrigin: true,
-        ws: true
+    ['SIGINT', 'SIGTERM'].forEach(function (sig) {
+      process.on(sig, function () {
+        devServer.close()
+        process.exit()
       })
-    devServer.use(mayProxy, hpm)
-
-    // Listen for the websocket 'upgrade' event and upgrade the connection.
-    // If this is not done, httpProxyMiddleware will not try to upgrade until
-    // an initial plain HTTP request is made.
-    devServer.listeningApp.on('upgrade', hpm.upgrade)
-  }
-
-  // Finally, by now we have certainly resolved the URL.
-  // It may be /index.html, so let the dev server try serving it again.
-  devServer.use(devServer.middleware)
-}
-
-const devServer = new WebpackDevServer(compiler, {
-  hot: true,
-  inline: true,
-  publicPath: '/',
-  quiet: true,
-  historyApiFallback: true
-})
-
-addMiddleware(devServer)
-
-// Launch WebpackDevServer.
-devServer.listen(port, function (err) {
-  if (err) {
-    return console.log(err)
-  }
-})
-
-openBrowser('http://localhost:' + port + '/')
+    })
+  })
+  .catch(err => {
+    if (err && err.message) {
+      console.log(err.message)
+    }
+    process.exit(1)
+  })
