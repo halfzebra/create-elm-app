37ed9711367a964960a69e82896077a11c5fe405
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-06-11 22:08:49 +0200

    style: Remove standard

--- config/env.js
+++ config/env.js
@@ -1,10 +1,8 @@
-function getClientEnvironment () {
-  return Object
-    .keys(process.env)
-    .reduce((acc, current) => {
-      acc[ `process.env.${current}` ] = `"${process.env[ current ]}"`
-      return acc
-    }, {})
+function getClientEnvironment() {
+  return Object.keys(process.env).reduce((acc, current) => {
+    acc[`process.env.${current}`] = `"${process.env[current]}"`;
+    return acc;
+  }, {});
 }
 
-module.exports = getClientEnvironment
+module.exports = getClientEnvironment;
