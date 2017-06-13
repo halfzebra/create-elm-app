9af9fe458cc56be0ac9577f69dd3f37f7e4986d9
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-06-13 21:39:44 +0200

    fix: Keep the files inside the module system in src folder

--- template/public/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../css/main.css';
-
-const logoPath = require('../media/logo.svg');
-const Elm = require('../../src/App.elm');
-
-const root = document.getElementById('root');
-
-Elm.App.embed(root, logoPath);
