91c59c47170bc65d142ff502a789cdb2f14e24a6
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-07-03 08:41:19 +0200

    chore: Added a comment, refering to the docs about elm-test

--- template/tests/Tests.elm
+++ template/tests/Tests.elm
@@ -5,6 +5,9 @@ import Expect
 import String
 
 
+-- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
+
+
 all : Test
 all =
     describe "A Test Suite"
