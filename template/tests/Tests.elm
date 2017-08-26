92737bdd504da18eb769122ba28157bb17d2f708
andys8 <andys8@users.noreply.github.com>
2017-08-26 10:48:39 +0200

    style($testing): Test template syntax using underscore
    Elm-Test recently changed the proposal how a test function should look like.
    
    166

--- template/tests/Tests.elm
+++ template/tests/Tests.elm
@@ -12,12 +12,12 @@ all : Test
 all =
     describe "A Test Suite"
         [ test "Addition" <|
-            \() ->
+            \_ ->
                 Expect.equal 10 (3 + 7)
         , test "String.left" <|
-            \() ->
+            \_ ->
                 Expect.equal "a" (String.left 1 "abcdefg")
         , test "This test should fail" <|
-            \() ->
+            \_ ->
                 Expect.fail "failed as expected!"
         ]
