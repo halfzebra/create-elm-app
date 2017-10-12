45077bc397dc188709228de05e4c02df380d7581
Jesper Håkansson <jesper@jesperh.se>
2017-10-12 20:10:47 +0200

    test: Add utility scripts test

--- tests/elm-app.eject.spec.js
+++ tests/elm-app.eject.spec.js
@@ -83,4 +83,17 @@ describe('Ejecting Elm application. (Please wait...)', () => {
     expect(status, 'to be', 0);
     expect(outputString, 'to contain', 'Compiled successfully');
   }).timeout(5 * 60 * 1000);
+
+  it('Ejected application should have utility scripts', () => {
+    expect(
+      fs.existsSync(path.join(testAppDir, './scripts/utils/formatElmCompilerErrors.js')),
+      'to be',
+      true
+    );
+    expect(
+      fs.existsSync(path.join(testAppDir, './scripts/utils/webpackHotDevClient.js')),
+      'to be',
+      true
+    );
+  });
 });
