d80ea4eb27d60162fb27792ea0aeca2920ce6376
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-10-18 03:15:38 +0200

    chore: remove duplicated tests from create-elm-app package

--- packages/create-elm-app/tests/fixtures/Main.elm
+++
@@ -1,9 +0,0 @@
-module Main exposing (..)
-
-import App exposing (..)
-import Html.App exposing (program)
-
-
-main : Program Never
-main 
-    program { view = view, init = init, update = update, subscriptions = subscriptions }
