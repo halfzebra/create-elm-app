ce5c6947c947aca8b07e15791927c3172aa167e2
Andy <andys8@users.noreply.github.com>
2018-11-21 00:10:44 +0100

    Runtime opts to improve build speed #321

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -176,6 +176,7 @@ module.exports = {
               // for invalid values, "true" and as a default, enable it
               debug: process.env.ELM_DEBUGGER === 'false' ? false : true,
               pathToElm: paths.elm,
+              runtimeOptions: '-A128M -H128M -n8m',
               forceWatch: true
             }
           }
