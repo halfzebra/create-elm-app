df55eb1ae6f8877b1daeedadb091bab920085130
Eduard Kyvenko <edky@tv2.dk>
2019-07-18 17:48:43 +0300

    docs: updated the link to elm-test package in test example

--- template/tests/Tests.elm
+++ template/tests/Tests.elm
@@ -4,7 +4,7 @@ import Test exposing (..)
 import Expect
 
 
--- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
+-- Check out https://package.elm-lang.org/packages/elm-explorations/test/latest to learn more about testing in Elm!
 
 
 all : Test
